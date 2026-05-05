d8345c32a9cdfe45aec30fade7c0adb7f4f15120
kxu111 <potatochipyum5@gmail.com>
2026-05-05 23:12:39 +1000

    fixes

--- .config/scripts/tmux-auto-attach.sh
+++ .config/scripts/tmux-auto-attach.sh
@@ -4,5 +4,4 @@ if ! command -v tmux &> /dev/null; then
     exit 0
 fi
 
-session_name="$(whoami)"
-tmux attach-session -t "$session_name" 2>/dev/null || tmux new-session -s "$session_name"
+tmux attach-session 2>/dev/null || tmux new-session -s "$(whoami)"
