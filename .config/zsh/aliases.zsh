9e9d3ac45aa6b7e212a944ba12fb3aa5f2513160
kxu111 <potatochipyum5@gmail.com>
2026-05-05 19:21:10 +1000

    merge tmux aliases
    a

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -21,3 +21,25 @@ alias la='eza -a'
 alias lla='eza -la'
 alias llh='eza -lh'
 alias llah='eza -lah'
+
+
+# tmux aliases
+alias tls='tmux ls'
+ta() {
+  if ! tmux has-session 2>/dev/null; then
+    tmux new-session -d -s "$(whoami)"
+  fi
+  [[ -n "$1" ]] && tmux attach -t "$1" || tmux attach
+}
+
+tkill() {
+  [[ -n "$1" ]] && tmux kill-session -t "$1" || tmux kill-session
+}
+
+_tmux_completion() {
+  local -a sessions
+  sessions=($(tmux list-sessions -F "#{session_name}" 2>/dev/null))
+  compadd -a sessions
+}
+compdef _tmux_completion ta
+compdef _tmux_completion tkill
--- .zshrc
+++ .zshrc
@@ -1,4 +1,3 @@
 source "$XDG_CONFIG_HOME/zsh/aliases.zsh"
-source "$XDG_CONFIG_HOME/zsh/tmux.zsh"
 source "$XDG_CONFIG_HOME/zsh/plugins.zsh"
 source "$XDG_CONFIG_HOME/zsh/prompt.zsh"
