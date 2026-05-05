ff93d7f7f36383c1d06ab5bdddf0e79ce9a3e446
kxu111 <potatochipyum5@gmail.com>
2026-05-05 19:26:44 +1000

    support vi mode in prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,1 +1,14 @@
 PROMPT='%~ $ '
+
+# color change for vi mode
+function zle-keymap-select { 
+    if [[ ${KEYMAP} == vicmd ]]; then
+        PROMPT='%F{magenta}%~ $ %f'
+    else
+        PROMPT='%F{white}%~ $ %f'
+    fi
+    zle reset-prompt
+}
+
+zle -N zle-keymap-select
+
