ac934c29c29698b14a1dc95c00c9bead0b3e6fdf
kxu111 <potatochipyum5@gmail.com>
2026-05-13 16:34:36 +1000

    cleanup + vi binds
    a

--- .config/zsh/plugconf.zsh
+++ .config/zsh/plugconf.zsh
@@ -1,9 +1,6 @@
 # fzf
 source <(fzf --zsh)
 export FZF_DEFAULT_OPTS_FILE="$HOME/.config/fzf/config"
-stty -ixon
-bindkey -r '^T'
-bindkey '^S' fzf-file-widget
 export FZF_CTRL_R_OPTS="--style=default --info=hidden --preview-window=hidden --no-scrollbar --tmux --scheme=history"
 export FZF_CTRL_T_OPTS="--style=default --info=hidden --preview-window=hidden --no-scrollbar --tmux --scheme=path"
 
--- .zshrc
+++ .zshrc
@@ -1,5 +1,6 @@
 source "$XDG_CONFIG_HOME/zsh/aliases.zsh"
 source "$XDG_CONFIG_HOME/zsh/plugconf.zsh"
+source "$XDG_CONFIG_HOME/zsh/binds.zsh"
 
 # always have a backup term in case this breaks
 if [[ "$TERM" == "xterm-ghostty" ]] && [[ -f ~/.config/scripts/auto-attach.sh ]]; then
