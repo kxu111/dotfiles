e01ef2cbeefdd341db393ab243ccbca6113acc43
Kenneth Xu <kenny@Kenneths-MacBook-Air.local>
2026-05-18 00:20:39 +1000

    install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# run this AFTER setting up nix-darwin
+cd ~/dotfiles
+mkdir ~/.config
+stow . --adopt
+desktoppr ~/Pictures/walls/sunflower.jpg
+bash <(curl -sSL https://spotx-official.github.io/run.sh)
+source ~/.zshenv
+source ~/.zshrc
+rustup default stable
+crontab ~/.config/cron/crontab
+bat cache --build
+echo "REMINDER TO DISABLE SPOTLIGHT AND SET UP ALFRED"
