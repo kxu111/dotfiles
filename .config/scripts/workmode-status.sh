1ddca9fa6c2c642660dbc9e0c7d84e0b62dd9777
kxu111 <potatochipyum5@gmail.com>
2026-06-24 23:55:25 +1000

    clean up tmux + more

--- .config/scripts/workmode-status.sh
+++ .config/scripts/workmode-status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -f /tmp/workmode-enabled ]; then
-    echo "#[reverse bold fg=colour15,bg=black]"
+    echo "#[reverse]"
 else
-    echo "#[bold fg=colour15,bg=default]"
+    echo ""
 fi
